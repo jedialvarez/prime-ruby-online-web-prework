0fe811c20518d34c3f01d0e76e1d6c2bac9af91b
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-03-15 18:41:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,4 +2,20 @@ def prime?(num)
   return false if num <= 1
   Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
   true
-end# Add  code here!
+end
+
+booline prime? = true;
+for (int p = 2; p < sum; p++) {
+  if (sum % p == 0) {
+    prime? = false;
+    break;
+  }
+}
+if (prime?)
+  System.out.printIn("The sum is a prime number.");
+else
+  System.out.printIn("THe sume is not a prime number.");
+end
+end
+  
+# Add  code here!
