bec113c49cf37f0020af8fe024b82b4e0039be12
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-03-15 18:43:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,18 @@
+// tests if n is prime
+ public static boolean isPrime(int n) {
+     if (n<2) return false;
+     for(int p = 2; p < Math.sqrt(n); p++) {
+        if(n % p == 0) return false;  // enough to find one devisor to show n is not a prime
+     }
+     return true; // no factors smaller than sqrt(n) were found
+ }
 
-public static boolean isPrime(int num) {
-
-    for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0) {
-            return false;
-        }
-    }
-
-    return true;
-}
+ public static void main(String []args){
+    ...
+    System.out.println("sum is "+ sum);
+    if (isPrime(sum)) 
+        System.out.println("The sum is a prime number.");
+    else 
+        System.out.println("The sum is not a prime number.");
+ }
 # Add  code here!
